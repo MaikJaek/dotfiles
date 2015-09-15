71616e06e7f305c510f71d2925da77561419e29c
Marcello Muscara <marcello.muscara@gmail.com>
2015-09-15 21:53:05 +0200

    aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -112,3 +112,6 @@ alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resource
 
 # Reload the shell (i.e. invoke as a login shell)
 alias reload="exec $SHELL -l"
+
+# Easy ps grep
+alias psg="ps u | grep "
