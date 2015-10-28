4ba27161d8366cc30bfe2dab155be0c7270e0e86
Marcello Muscara <marcello.muscara@gmail.com>
2015-10-28 18:56:52 +0100

    update

--- shell/env.sh
+++ shell/env.sh
@@ -4,8 +4,9 @@ export HIST_STAMPS="dd.mm.yy"
 export DEFAULT_USER=marcellomuscara
 
 # use coreutils instead of osx ones
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/bin/:/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 
 # use home .bin folder
 export PATH="~/.bin:$PATH"
 
+export XSITE_ENV=LOCAL_MM_SACOM_MIG
